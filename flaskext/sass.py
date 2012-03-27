1413f6403f3b630e43ff61df08ffb268b768bb34
Christopher Rogers <crogers@iseatz.com>
2012-03-27 14:19:49 -0500

    Adding condition to skip partials
    Signed-off-by: Christopher Rogers <crogers@iseatz.com>

--- flaskext/sass.py
+++ flaskext/sass.py
@@ -90,6 +90,7 @@ class Sass(object):
         log.debug("Finding *.{sass,scss} files under %s", search_path)
         for path, subdirs, filenames in os.walk(search_path):
             for f in filenames:
+                continue if f.beginswith('_')
                 if f.endswith('.sass'):
                     yield (
                         os.path.join(path, f),
